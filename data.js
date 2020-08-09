3a4977f6e7836d9794fa8f16f5e65c8b83f8496b
ahmdaini <ahmd,aini@gmail.com>
2020-08-09 11:54:21 +0800

    First Commit
    This is my first try, dont judge me ok? Thankies... :D

---
+++ data.js
@@ -0,0 +1,123 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-panorama-lumion-jua",
+      "name": "panorama lumion jua",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4367883051995456,
+          "pitch": -0.016251999832331165,
+          "rotation": 0,
+          "target": "1-panorama_my_room"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.27134553459064925,
+          "pitch": -0.44622830037454264,
+          "title": "Cabin in The Woods",
+          "text": "A small cabin, tepat guna pokoknya..."
+        },
+        {
+          "yaw": 1.4422869514348244,
+          "pitch": 0.0549310029445671,
+          "title": "Mobil Range Rover",
+          "text": "Tipikal mobil anak-anak sultan tajir."
+        }
+      ]
+    },
+    {
+      "id": "1-panorama_my_room",
+      "name": "panorama_my_room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6840477923298174,
+          "pitch": 0.047595800935551935,
+          "rotation": 0,
+          "target": "0-panorama-lumion-jua"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.6258461668678574,
+          "pitch": -0.5212310884105307,
+          "title": "Gantungan Baju",
+          "text": "Konon baju yang digantung disini sudah bertahun-tahun tidak dicuci..."
+        },
+        {
+          "yaw": 0.5043040625929258,
+          "pitch": -0.5184505839508144,
+          "title": "Jam Dinding Minecraft",
+          "text": "Gambarnya di custom supaya style Minecraft"
+        },
+        {
+          "yaw": -0.15724054286931022,
+          "pitch": 0.0009859329797556882,
+          "title": "Laptop",
+          "text": "Laptop yang selalu menyala"
+        },
+        {
+          "yaw": 0.5967718438212088,
+          "pitch": -0.8654995464183219,
+          "title": "Skylight",
+          "text": "Lets in some light from above."
+        },
+        {
+          "yaw": 1.7088089016516808,
+          "pitch": 0.3101231708337586,
+          "title": "Bantal",
+          "text": "Bantal ini sudah lama sekali tidak dijemur... BAU HAPAK"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
